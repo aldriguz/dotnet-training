bf13718b1b23361b9dd1b095697115a1b572c129
Kevin Alama <alama1992@gmail.com>
2020-07-01 23:08:11 -0500

    basis for types

---
+++ DotNetTraining/Basis/TypesDeclaration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetTraining.Basis
+{
+    class TypesDeclaration
+    {
+        //Explore multiple ways to declare data types and print on console
+
+        public void displayStrings() 
+        {
+            string emptyValue = null;
+            string nonEmptyValue = "my favorite movie";
+
+            Console.WriteLine("Length:" + emptyValue.Length);
+            Console.WriteLine("Length:" + nonEmptyValue!.Length) ;
+        }
+        
+
+
+    }
+}
