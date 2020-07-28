c2071909dc0c3c8849513586e38234a2df9a0edc
Kevin Alama <alama1992@gmail.com>
2020-07-27 20:08:24 -0500

    create new project from console

---
+++ poo.concepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poo.concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
