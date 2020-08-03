4d5203db30a77ef18797c87264313fc3e15162a4
Kevin Alama <alama1992@gmail.com>
2020-08-03 00:47:49 -0500

    linq introduction

---
+++ linq.introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace linq.introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IList<string> universities = new List<string>() {
+                "Oxford - USA",
+                "Standford - USA",
+                "UNT - Perú",
+                "UPN - Perú",
+                "Universidad Alas Peruanas - Perú",
+                "MIT - USA"
+            };
+
+            var nationalUniversities = from u in universities
+                                        where u.Contains("Perú")
+                                        select u;
+
+
+            Console.WriteLine(nationalUniversities.Join(","));
+        }
+    }
+}
