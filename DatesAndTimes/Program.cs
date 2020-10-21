6680d4d73cfc641c6964cf25ba77b146aa6ae43a
Kevin Alama <alama1992@gmail.com>
2020-10-21 13:10:32 -0500

    date and times training

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //utc
+            var contractDate = new DateTimeOffset(2020, 10, 21, 13, 05, 20, TimeSpan.Zero);
+
+            Console.WriteLine(contractDate);
+            
+            //extending 5 months
+            //getting end date
+            var expirationContractDate = contractDate.AddMonths(5).AddTicks(-1);
+
+            Console.WriteLine(expirationContractDate);
+
+        }
+    }
+}
