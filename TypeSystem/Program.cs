39e2c06b61adcc955d483f365ffcb2fc5b85d616
Kevin Alama <alama1992@gmail.com>
2021-08-04 09:31:19 -0500

    add new projects

---
+++ TypeSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
