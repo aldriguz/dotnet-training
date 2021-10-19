82ffc49522fb167af408cdc9e346b53604800bf1
Kevin Alama <alama1992@gmail.com>
2021-10-19 17:26:57 -0500

    some code of console class

---
+++ ConsoleExplore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow; //background text color
+            Console.ForegroundColor = ConsoleColor.Red; //text color
+            Console.Title = "Personalizado";
+            Console.WriteLine($"Tamaño de ventana: {Console.WindowHeight}x{Console.WindowWidth}");
+            string input = "";
+
+            do
+            {
+                Console.Write("Escribe (q) o (Q) para salir:");
+                input = Console.ReadLine();
+            } while (input != "q" && input != "Q");
+        }
+    }
+}
