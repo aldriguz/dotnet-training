5320e9ec55cef343da2842dcabfcb41c6a874dcc
Kevin Alama <alama1992@gmail.com>
2021-10-26 17:06:03 -0500

    abstract code (ba dum ts)

---
+++ poo.concepts/OlimpoAnimals/Concept/MiticalAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo.concepts.OlimpoAnimals.Concept
+{
+    //this is an ABSTRACT concept, because we need to derive in other clases
+    public abstract class MiticalAnimal
+    {
+        private string NickName { get; set; }
+        public void SetNickname(string nickName) => NickName = nickName;
+        public string GetNickname() => NickName;
+
+        //need to provide an implementation inderived classes
+        public abstract string GetMiticalSound();
+
+    }
+}
---
+++ poo.concepts/OlimpoAnimals/Pegaso.cs
@@ -0,0 +1,16 @@
+﻿using poo.concepts.OlimpoAnimals.Concept;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo.concepts.OlimpoAnimals
+{
+    public class Pegaso : MiticalAnimal
+    {
+        public override string GetMiticalSound()
+        {
+            //don't know what is the sound that a pegaso makes xD
+            return "Mitical hourse sound!!";
+        }
+    }
+}
