6a45177f37707f9b8843e293cd2fe590480aa656
Kevin Alama <alama1992@gmail.com>
2021-10-26 16:57:43 -0500

    sealed class interaction

---
+++ poo.concepts/Inheritance/Departament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo.concepts.Inheritance
+{
+    public sealed class Departament
+    {
+        //this class can not be used for inheritance
+        public decimal AreaOfBuilding { get; set; }
+    }
+
+    // this for example is invalid
+    //public sealed class SuiteDepartament : Departament { }
+}
