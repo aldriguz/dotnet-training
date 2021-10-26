e1efe2a3553356423f5639705979e869f14bb87c
Kevin Alama <alama1992@gmail.com>
2021-10-25 23:25:23 -0500

    garbage collector demo

---
+++ GarbageCollector/MemoryKiller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    public class MemoryKiller
+    {
+        public MemoryKiller(Int32 id, DateTime createdDate, Int32 maxCapacity)
+        {
+            this.Id = id;
+            this.CreatedDate = createdDate;
+            this.MaxCapacity = maxCapacity;
+        }
+
+        public Int32 Id { get; set; }
+        private DateTime _createdDate;
+
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+        private Int32 _maxCapacity;
+
+        public Int32 MaxCapacity
+        {
+            get { return _maxCapacity; }
+            set
+            {
+                if (value < 0)
+                    _maxCapacity = 0;
+                else
+                    _maxCapacity = value;
+            }
+        }
+
+    }
+}
---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MemoryKiller> lstMemory = new List<MemoryKiller>();
+            
+            //Double amount = 1 000 000M;
+            
+            for (int i = 0; i < 100000000; i++)
+            {
+                lstMemory.Add(new MemoryKiller(1, DateTime.Now, 100));
+            }
+
+            lstMemory.Clear();
+            lstMemory = null;
+
+            GC.Collect(); //this line runs garbage collector to free memory
+            
+            Console.WriteLine("Press key to quit...");
+            Console.ReadLine();
+        }
+    }
+}
