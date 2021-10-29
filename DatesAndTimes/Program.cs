40836aa9bea3d9cacedf8852feaebab2c4eae170
Kevin Alama <alama1992@gmail.com>
2021-10-29 14:56:14 -0500

    some date formats to play like YY

--- DatesAndTimes/Program.cs
+++ DatesAndTimes/Program.cs
@@ -17,7 +17,8 @@ namespace DatesAndTimes
                 Console.WriteLine(ex.Message);
             }
             
-            
+            var yearFormatYY = DateTime.Now.Year%100;
+            var customYearFormatYY = 2024 % 100;
 
             //utc
             var contractDate = new DateTimeOffset(2020, 10, 21, 13, 05, 20, TimeSpan.Zero);
