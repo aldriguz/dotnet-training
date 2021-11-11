fdc639709e073eaa574d4e6b03fbf31cd42db1b0
Kevin Alama <alama1992@gmail.com>
2021-11-10 20:23:49 -0500

    simple stack to review generics

---
+++ TryingGenerics/BasicDataStructure/SimpleStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingGenerics.BasicDataStructure
+{
+    internal class SimpleStack
+    {
+        private readonly double[] _array;
+        private int _index;
+
+        public SimpleStack(int quantity)
+        {
+            _index = -1;
+            _array = new double[quantity];
+        }
+
+        public double Pop() => _array[_index--];
+        public void Push(double value) => _array[++_index] = value;
+        public int Count => _index + 1;
+    }
+}
---
+++ TryingGenerics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TryingGenerics.BasicDataStructure;
+
+namespace TryingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleStack stack = new SimpleStack(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            Console.WriteLine("Program finished!");
+        }
+    }
+}
