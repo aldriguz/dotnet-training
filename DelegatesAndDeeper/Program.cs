1f8843f349eb76a3fa3748fc5bfbe43edd6816a3
Kevin Alama <alama1992@gmail.com>
2021-11-13 23:09:12 -0500

    Adding projects for delegates and project for new features for net 6

---
+++ DelegatesAndDeeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesAndDeeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
