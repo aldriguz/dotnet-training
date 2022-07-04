3639b7773dffa81e47c68135db8ad7f916f78074
Kevin Alama <alama1992@gmail.com>
2022-07-03 21:23:38 -0500

    xml reader test

---
+++ streams-io/XmlReader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Settings for reader
+XmlReaderSettings settings = new XmlReaderSettings();
+settings.IgnoreWhitespace = true;
+settings.ConformanceLevel = ConformanceLevel.Fragment;
+
+// Reading the file
+using XmlReader reader = XmlReader.Create("XMLFile1.xml", settings);
+
+reader.MoveToContent();
+reader.ReadStartElement("catalog");
+
+XmlElement element;
+XmlNode node;
+XmlAttribute atribute;
