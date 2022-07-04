383d4a55031a52f8cfe88071607d4ae9bb43b6e3
Kevin Alama <alama1992@gmail.com>
2022-07-03 21:29:54 -0500

    project for exploring XmlDocument class

---
+++ streams-io/XmlDocument/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlDocumentProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ streams-io/XmlDocument/XmlProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace XmlDocumentProcessor
+{    
+    internal class XmlProcessor
+    {
+        private readonly XmlDocument _xmlDocument;
+        public XmlProcessor(string file)
+        {
+            if (File.Exists(file))
+                throw new FileNotFoundException();
+
+            _xmlDocument = new XmlDocument();
+            _xmlDocument.LoadXml(file);
+        }
+
+        public void Process()
+        {
+
+        }
+    }
+}
