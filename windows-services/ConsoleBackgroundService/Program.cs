3b17cbde3a7161ff8c9045904fd7f850614b064e
Kevin Alama <alama1992@gmail.com>
2022-07-04 21:09:54 -0500

    playing with working services: from scratch and dotnet project template

---
+++ windows-services/ConsoleBackgroundService/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleBackgroundService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder().Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices(
+                    (ctx, services) =>
+                    {
+                        services.AddHostedService<WorkerService>();
+                    });
+        }
+    }
+}
---
+++ windows-services/ConsoleBackgroundService/WorkerService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleBackgroundService
+{
+    internal class WorkerService : BackgroundService
+    {
+        private readonly ILogger<WorkerService> _logger;
+        public WorkerService(ILogger<WorkerService> logger)
+        {
+            this._logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                _logger.Log(LogLevel.Information, $"Running service - current time {DateTime.Now}");
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
