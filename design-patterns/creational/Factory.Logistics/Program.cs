8c3bc478343a991c7e32b2037a6a4ff0de289327
Kevin Alama <alama1992@gmail.com>
2022-07-07 21:22:03 -0500

    design patterns faactory

---
+++ design-patterns/creational/Factory.Logistics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory.Logistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
