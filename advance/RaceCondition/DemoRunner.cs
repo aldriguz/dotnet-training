af7090256803cb6f7dd7782dd9488c4fc1d809db
Kevin Alama <alama1992@gmail.com>
2022-07-15 08:22:37 -0500

    race condition examples

---
+++ advance/RaceCondition/DemoRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceCondition;
+
+public class DemoRunner
+{
+    int resultValue = 1;
+
+    void Work1() { resultValue = 1; }
+    void Work2() { resultValue = 2; }
+    void Work3() { resultValue = 3; }
+
+    public static void Process()
+    {
+        DemoRunner runner = new DemoRunner();
+        Thread worker1 = new Thread(runner.Work1);
+        Thread worker2 = new Thread(runner.Work2);
+        Thread worker3 = new Thread(runner.Work3);
+
+        worker1.Start();
+        worker2.Start();
+        worker3.Start();
+
+        Console.WriteLine(runner.resultValue);
+        Console.Read();
+    }
+}
---
+++ advance/RaceCondition/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RaceCondition;
+
+DemoRunner.Process();
