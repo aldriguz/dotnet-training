a4ff81304b7883c1a3014e0b0ff4d16b7c4eea0d
Kevin Alama <alama1992@gmail.com>
2022-09-27 21:53:00 -0500

    adding different types of contracts

---
+++ fundamentals/Interfaces/Abstracts/AbstractPolygon.cs
@@ -0,0 +1,11 @@
+
+namespace Interfaces.Abstracts;
+
+public abstract class AbstractPolygon 
+{
+    public int SidesNumber { get; set; }
+    public int SideLength { get; set; }
+
+    public abstract double CalculateArea();
+    public abstract double CalculatePerimeter();
+}
---
+++ fundamentals/Interfaces/Interfaces/IPolygon.cs
@@ -0,0 +1,11 @@
+
+namespace Interfaces.Interfaces;
+
+public interface IPolygon 
+{
+    int SidesNumber { get; set; }
+    int SideLength { get; set; }
+
+    double CalculateArea();
+    double CalculatePerimeter();
+}
