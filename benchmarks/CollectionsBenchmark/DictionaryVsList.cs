f1ab6ae88aa247766e2e5d06ce39afed4134d272
Kevin Alama <alama1992@gmail.com>
2022-12-30 09:38:30 -0500

    solution updates

---
+++ benchmarks/CollectionsBenchmark/DictionaryVsList.cs
@@ -0,0 +1,9 @@
+namespace Collecttionsbenchmark;
+
+public class DictionaryVsList
+{
+    private const int N = 10000;
+    private readonly int[] data;
+    
+
+}
---
+++ oop/GenericsBasics/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Crear una clase que emule lo de intelliflo y sus clases, instancia de una clase de orden inferior a nivel de herencia e intentar usar una propiedad que no esta disponible
+// Se puede instanciar una clase de orden menor?
