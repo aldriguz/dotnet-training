e6602e88385949a3af5583f7e1b565a6118d54c7
Kevin Alama <alama1992@gmail.com>
2024-09-23 12:19:31 -0500

    WIP Binary expression

---
+++ advanced/BinaryExpression/BinaryExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BinaryExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Specification 
+        }
+    }
+}
---
+++ advanced/BinaryExpression/BinaryExpression/Specification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExpression
+{
+    public class CircleArea<T> where T : int, decimal
+    {
+        private readonly const decimal pi = 3.14159265358979323846;
+        private readonly T radius;
+        public Expresion<Func<T>> area = () => area * pi;
+    }
+}
